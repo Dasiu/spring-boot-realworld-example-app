5c3ff900ee66a9cf5f4ad70f98a3939bfe951b3c
Adam Butkiewicz <adam.butkiewicz@enterprisesoftware.pl>
2018-01-08 15:49:16 +0100

    Poprawiono literowke w typie kolumny

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,7 +1,7 @@
 create table users (
   id varchar(255) primary key,
   username varchar(255) UNIQUE,
-  password varchar(1),
+  password varchar(255),
   email varchar(255) UNIQUE,
   bio text,
   image varchar(511)
