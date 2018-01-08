b90d23ce0929991faba968ffbd583978d6d7bcba
Adam Butkiewicz <adam.butkiewicz@enterprisesoftware.pl>
2018-01-08 07:38:26 +0100

    Planted error in sql migration

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,7 +1,7 @@
 create table users (
   id varchar(255) primary key,
   username varchar(255) UNIQUE,
-  password varchar(255),
+  password varchar(1),
   email varchar(255) UNIQUE,
   bio text,
   image varchar(511)
